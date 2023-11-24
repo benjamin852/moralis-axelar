aaa5df69df4157b5e1e7ec41331c358c2a3dbf03
benjamin852 <weinbergben33@gmail.com>
2023-11-24 15:25:33 +0100

    keys

--- pages/api/moralis/[...moralis].ts
+++ pages/api/moralis/[...moralis].ts
@@ -1,9 +1,9 @@
 import { MoralisNextApi } from '@moralisweb3/next';
 
-const MORALIS_API_KEY = 'JSubmrWUErdQR3QUTSztwNzpfO84QhKL6b5IFo5u9FzH2VsvBt4LEoQpsOqa56ZU';
+const MORALIS_API_KEY = process.env.MORALIS_API_KEY || '';
 const NEXTAUTH_URL = 'http://localhost:3000';
 export default MoralisNextApi({
-  apiKey: 'JSubmrWUErdQR3QUTSztwNzpfO84QhKL6b5IFo5u9FzH2VsvBt4LEoQpsOqa56ZU',
+  apiKey: MORALIS_API_KEY,
   authentication: {
     domain: NEXTAUTH_URL ? new URL(NEXTAUTH_URL).host : '',
     uri: 'http://localhost:3000',
